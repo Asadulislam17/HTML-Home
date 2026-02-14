17aaa9224f10b89790a8958c8f0510b6e3f84053
Asadulislam17 <asadulislam227940@gmail.com>
2026-02-14 11:59:17 +0600

    update

---
+++ Home/JAVASCRIPT/VARIABLE/script.js
@@ -0,0 +1,9 @@
+var a='hello';
+a="Hello World";
+document.write(typeof(a)+"<br>");
+document.write(a+"<br>");
+let b='hi';
+// a=" World";
+document.write(typeof(b)+"<br>");
+document.write(b);
+document.write(20+40)
